57957b96a6d94e739dd24f3427da9020757e83dc
Arnavv77 <arnavvg77@gmail.com>
2026-07-08 15:21:08 +0530

    Time: 2 ms (3.54%) | Memory: 46.7 MB (12.66%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,37 +1,9 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] == target) {
-                return true;
-            }
-            
-            // Edge case: when duplicates make it impossible to determine the sorted half
-            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
-                left++;
-                right--;
-            }
-            // Left half is sorted
-            else if (nums[left] <= nums[mid]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1; // Target is in the left half
-                } else {
-                    left = mid + 1;  // Target is in the right half
-                }
-            } 
-            // Right half is sorted
-            else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;  // Target is in the right half
-                } else {
-                    right = mid - 1; // Target is in the left half
-                }
-            }
+        HashSet<Integer> m=new HashSet<>();    // using HashSet
+        for(int i=0;i<nums.length;i++){
+            m.add(nums[i]);             // Built-in Java function to add the numbers directly into Set
         }
-        
-        return false;
+        return m.contains(target);      // Same built-in function to search if element is there
     }
 }
